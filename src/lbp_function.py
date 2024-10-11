d0f8a8dbd1ccb2fd402d96461724f03e132a2d92
Amir Hossein Eshghi <17421278+amir1323@users.noreply.github.com>
2024-10-11 18:22:13 +0330

    Create lbp_function.py

---
+++ src/lbp_function.py
@@ -0,0 +1,36 @@
+# lbp_function.py
+import numpy as np
+from bilinear_interpolation import bilinear_interpolation
+
+def circ_classical_lbp(img, y, x, r, p):
+    """
+    Compute the circular classical Local Binary Pattern (LBP).
+    
+    Parameters:
+        img (numpy array): Input grayscale image.
+        y (int): Y-coordinate of the pixel.
+        x (int): X-coordinate of the pixel.
+        r (int): Radius for LBP.
+        p (int): Number of neighbors.
+        
+    Returns:
+        int: LBP value for the central pixel.
+    """
+    binary_color = 0
+    center = img[y, x]
+    angles = np.linspace(0, 2 * np.pi, p + 1)[:-1]
+
+    for angle in angles:
+        y_point1 = (y - ((r - 1) * np.sin(angle)))
+        x_point1 = (x + ((r - 1) * np.cos(angle)))
+        y_point2 = (y - (r * np.sin(angle)))
+        x_point2 = (x + (r * np.cos(angle)))
+        
+        newval1 = bilinear_interpolation(img, y_point1, x_point1)
+        newval2 = bilinear_interpolation(img, y_point2, x_point2)
+        p_value = np.uint8(round((newval1 + newval2) / 2, 0))
+        
+        binary_color <<= 1
+        binary_color |= (p_value >= center)
+
+    return binary_color
